7407d86db75841e6945bd03b83724be6137ad675
agumusay <a.gumusay@protonmail.com>
2020-03-23 21:03:26 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./styles/App.scss";
 import data from "./data/data.json";
 
+//Logos
+import phoneLogo from "./assets/phone.svg";
+import mailLogo from "./assets/mail.svg";
+import webLogo from "./assets/xing-website-logo.svg";
 function App() {
   return (
     <div className="App">
@@ -11,28 +15,28 @@ function App() {
           return (
             <li key={person.id}>
               <div className="head">
-                <img src={person.avatar} alt="" />
+                <img src={person.avatar} alt={person.firstName + " " + person.lastName} />
                 <div className="name-container">
                   <div className="name">
-                    {person.firstName} {person.lastName}{" "}
+                    {person.firstName} {person.lastName}
                     {person.isAdmin ? <span>Admin</span> : null}
                   </div>
                   <p className="title">{person.title}</p>
                 </div>
               </div>
               <div className="foot">
-                <div className="phone">
-                  <img src="" alt="" />
+                <a className="phone" href={"tel:" + person.contact.phone}>
+                  <img src={phoneLogo} alt="" />
                   <p className="phone">{person.contact.phone}</p>
-                </div>
-                <div className="e-mail">
-                  <img src="" alt="" />
+                </a>
+                <a className="e-mail" href={"mailto:" + person.contact.email}>
+                  <img src={mailLogo} alt="" />
                   <p className="email">{person.contact.email}</p>
-                </div>
-                <div className="website">
-                  <img src="" alt="" />
+                </a>
+                <a className="website" href={person.contact.url}>
+                  <img src={webLogo} alt="" />
                   <p className="website">{person.contact.url}</p>
-                </div>
+                </a>
               </div>
               <div className="date">
                 member since {new Date(person.memberSince).toLocaleDateString("de")}
